6110472473cfe0c05c2cb45468e4d1de7d9ff204
Ukinaa <aaniku77@gmail.com>
2022-05-13 22:36:09 -0300

    patched ft_strlcat.c

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -7,7 +7,7 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 
 	i = ft_strlen(dst);
 	j = 0;
-	while (*(src + j) && i + j < size)
+	while (src[j] && i + j < size)
 	{
 		dst[i + j] = src[j];
 		j++;
