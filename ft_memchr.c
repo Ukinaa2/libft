2a97309ab1c7e8011ed7fd9e9172ec3ea6eebaaa
Ukinaa <aaniku77@gmail.com>
2022-05-14 23:38:44 -0300

    removed main

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: gguedes <gguedes@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/14 23:22:42 by gguedes           #+#    #+#             */
-/*   Updated: 2022/05/14 23:37:35 by gguedes          ###   ########.fr       */
+/*   Updated: 2022/05/14 23:38:34 by gguedes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,16 +25,4 @@ void	*ft_memchr(const void *s, int c, size_t n)
 		return ((void *)s + i);
 	else
 		return (0);
-}
-
-#include <stdio.h>
-
-int	main(void)
-{
-	char	s1[] = ".abcdefjsadjsaaa";
-	void	*p;
-
-	p = ft_memchr(s1, '.', 0);
-	printf("%s\n", (char *)p);
-	return (0);
 }
