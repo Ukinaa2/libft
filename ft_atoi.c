fc62806788da833fbf383ced5760a0a2967e73fd
Ukinaa <aaniku77@gmail.com>
2022-05-15 22:13:04 -0300

    added ft_atoi.c

---
+++ ft_atoi.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gguedes <gguedes@student.42.rio>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/15 12:54:16 by gguedes           #+#    #+#             */
+/*   Updated: 2022/05/15 22:11:45 by gguedes          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int ft_atoi(const char *nptr)
+{
+	int	nb;
+	int negative;
+
+	nb = 0;
+	negative = 1;
+	while (*nptr == 32 || (*nptr >= 9 && *nptr <= 13))
+		nptr++;
+	if (*nptr == '-')
+		negative = 1;
+	if (*nptr == '-' || *nptr == '+')
+		nptr++;
+	while (*nptr >= '0' && *nptr <= '9')
+	{
+		nb = nb * 10 + *nptr - '0';
+		nptr++;
+	}
+	return (nb * negative);
+}
