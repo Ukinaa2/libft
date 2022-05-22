f641032ebc93574d4737b2533e57c3f2912cd197
gguedes <gguedes@student.42.rio>
2022-05-22 20:49:00 -0300

    bonus, deleted README.md

---
+++ ft_lstadd_back_bonus.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back_bonus.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gguedes <gguedes@student.42.rio>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/21 13:48:06 by gguedes           #+#    #+#             */
+/*   Updated: 2022/05/22 20:20:40 by gguedes          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*last;
+
+	if (!(*lst))
+		*lst = new;
+	else
+	{
+		last = ft_lstlast(*lst);
+		last->next = new;
+	}
+}
---
+++ ft_lstmap_bonus.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap_bonus.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gguedes <gguedes@student.42.rio>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/21 22:12:35 by gguedes           #+#    #+#             */
+/*   Updated: 2022/05/22 20:40:22 by gguedes          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*newlst;
+	t_list	*newnode;
+
+	if (!lst || !f || !del)
+		return (0);
+	newlst = 0;
+	while (lst)
+	{
+		newnode = ft_lstnew((*f)(lst->content));
+		if (!newnode)
+		{
+			ft_lstclear(&newlst, del);
+			return (0);
+		}
+		ft_lstadd_back(&newlst, newnode);
+		lst = lst->next;
+	}
+	return (newlst);
+}
