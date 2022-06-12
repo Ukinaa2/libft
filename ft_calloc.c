8bf022a146234ad2e5d9690092041c59811348eb
gguedes <gguedes@student.42.rio>
2022-06-12 19:42:47 -0300

    ft_calloc.c

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: gguedes <gguedes@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/05 11:30:34 by gguedes           #+#    #+#             */
-/*   Updated: 2022/06/09 18:55:52 by gguedes          ###   ########.fr       */
+/*   Updated: 2022/06/12 19:42:34 by gguedes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,6 @@ void	*ft_calloc(size_t num, size_t size)
 {
 	void	*p;
 
-	if (num && size)
-		if (num / (size * num) == size)
-			return (NULL);
 	p = malloc(num * size);
 	if (!p)
 		return (NULL);
